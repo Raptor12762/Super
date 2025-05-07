1e2c7aa951fc309674d5929e4f38eac9c12a2c77
BasketballKing11 <tt20winn@gmail.com>
2025-05-07 15:34:21 +0000

    progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,16 @@
-<h1>This is a test</h1>
-<p>Like so</p>
+<svelte:head>
+    <title>Super</title>
+</svelte:head>
+<nav>
+    <ul>
+        <li>
+            <a href="+page.svelte">Testing</a>
+        </li>
+        <li>
+            <a href="#">Hell</a>
+        </li>
+        <li>
+            <a href="#">NOOOOOOO</a>
+        </li>
+    </ul>
+</nav>
