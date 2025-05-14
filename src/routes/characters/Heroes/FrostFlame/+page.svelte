a620f46285ddd6bc85c76f5c2288795f614be900
Raptor12762 <200654545+Raptor12762@users.noreply.github.com>
2025-05-14 13:34:16 +0000

    Frost Flame text

--- src/routes/characters/Heroes/FrostFlame/+page.svelte
+++ src/routes/characters/Heroes/FrostFlame/+page.svelte
@@ -169,7 +169,21 @@
 <!--nice-->
 
 <div class="grid grid-flow-col w-full grid-rows-3 gap-4">
-  <div class="row-span-3 ml-4 mt-4 mb-4 bg-amber-900">01</div>
-  <div class="col-span-2 mr-4 mt-4 bg-amber-600">02</div>
-  <div class="col-span-2 row-span-2 mr-4 mb-4 bg-amber-300">03</div>
+  <div class="row-span-3 ml-4 mt-4 mb-4 bg-amber-900">Frostflame is more of a unique hero compared to the rest. Lacking most features from other mugs, such as a handle and some general liquid inside of the mug, it is unknown how frostflame is currently standing without any sustenance inside of his mug. 
+	<br>
+	<br>
+	Frostflame’s flames do not provide the same heat like a normal fire would. Instead, the flames provide a “chilling” sensation, akin to being sick due to a fever or similar illness. His flames generally are always active, (as depicted from the constant flame on his head,) but can be controlled in any way he deems necessary. His flames can also solidify whenever he chooses, which turns the flames into ice. 
+  </div>
+  <div class="col-span-2 mr-4 mt-4 bg-amber-600">007 <!--Don't know what we would replace this with, but i'm leaving it here just in case. Feel free to rearrange the places of the current text--></div>
+  <div class="col-span-2 row-span-2 mr-4 mb-4 bg-amber-300">Frost Flame has the ability to create a freezing frost fire. Though blue in a flame typically means it's hotter, in this case, it's much colder.
+	<br>
+	<br>
+	Frost Flame has the ability to manipulate frozen fire to his liking and oftentimes uses it to light dark rooms or create walls or even just to cover territory by freezing the ground to make it slippery.
+	<br>
+	<br>
+	Frostflame can imbue any object he’s holding with his frostflame, enhancing whatever he may be holding at the time. This causes the item he’s holding to be on fire, similar to the flame coming from his head. 
+	<br>
+	<br>
+	Frost Flame also has the ability to solidify himself into solid ice, essentially freezing him in place.
+  </div>
 </div>
