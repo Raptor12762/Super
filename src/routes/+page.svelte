f93aa404cc46462f1114f7765a973c7575c14f27
BasketballKing11 <tt20winn@gmail.com>
2025-05-15 16:52:35 +0000

    Main Page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,36 +2,25 @@
 	import { Flex, Frame, Header } from 'sk-clib';
 </script>
 
-<!--Drawer, copy and past the stuff in the ts script (and the script too if not already done) to put it on other pages-->
+<Flex col fill centerx class="gap-20 pt-4">
+	<div class="grid h-[45%] w-full">
+		<div class="mx-auto flex w-[75%] rounded-xl bg-purple-600">
+			<h1 class="font-bold text-white">Mug Corp</h1>
+		</div>
+	</div>
 
-<Flex fill col centerx class="box-border gap-20 p-10">
-	<Frame flex row fillh class="w-[60%] rounded-md bg-red-200">
-		<h1 class="mx-auto pt-6 text-5xl font-bold">Mug Corp</h1>
-	</Frame>
-	<!--Grid which shows the heros and villains, go this is pain-->
-	<!--I agree, this is pain-->
-	<Frame row fill class="root-grid-box grid gap-x-5 rounded-md">
-		<Flex fill center class="col-span-4 row-start-1 bg-white">
-			<Header bold xxl>Heros</Header>
-		</Flex>
+	<h1 class="text-center text-5xl font-bold">Mugs</h1>
 
-		<Frame class="col-span-1 row-start-2 bg-orange-600"></Frame>
+	<div class="grid h-[45%] w-fit grid-cols-3 gap-40">
+		<div class="mx-auto flex w-[20vw] rounded-xl bg-purple-600"></div>
+		<div class="mx-auto flex w-[20vw] rounded-xl bg-purple-600"></div>
+		<div class="mx-auto flex w-[20vw] rounded-xl bg-purple-600"></div>
+	</div>
 
-		<Frame class="col-span-1 col-start-2 row-start-2 bg-orange-600"></Frame>
+	<h1 class="text-center text-5xl font-bold">Teapots</h1>
 
-		<Frame class="col-span-1 col-start-3 row-start-2 bg-orange-600"></Frame>
-
-		<Frame class="col-span-1 col-start-4 row-start-2 bg-orange-600"></Frame>
-
-		<Flex fill center class="col-span-4 row-start-3 bg-white">
-			<Header bold xxl>Villians</Header>
-		</Flex>
-	</Frame>
+	<div class="grid h-[45%] w-fit grid-cols-2 gap-40 pb-4">
+		<div class="mx-auto flex w-[20vw] rounded-xl bg-purple-600"></div>
+		<div class="mx-auto flex w-[20vw] rounded-xl bg-purple-600"></div>
+	</div>
 </Flex>
-
-<style lang="postcss">
-	:global(.root-grid-box) {
-		grid-template-columns: 1fr 1fr 1fr 1fr;
-		grid-template-rows: 50px 1fr 50px 1fr;
-	}
-</style>
