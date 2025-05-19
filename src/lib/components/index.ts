fea79e6c9a5d4a282f5b004f3356882cff4682c3
trelta <treltasev@gmail.com>
2025-05-19 18:44:19 -0400

    feat(particles): Added Particle Component
    Its not too hard to used, also not scuffed :)

--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -4,3 +4,4 @@ export { default as Separator } from './separator/components/separator.svelte';
 export { default as DrawerRendered } from "./drawer_rendered/drawer_rendered_component.svelte"
 export * as Portrait from "./portrait"
 export * as PaperPs from "./paper-ps"
+export * as Particle from "./particle"
---
+++ src/lib/components/particle/index.ts
@@ -0,0 +1,7 @@
+export {default as _, default as Root} from "./components/particle.svelte";
+
+export { default as Generator } from "./components/particle-generator.svelte";
+export type {
+    tParticleProps as Props,
+    tParticleGeneratorProps as GeneratorProps,
+} from "./types"
