36c431f2517a4ba76aa41bf7684bc0e070965ecc
Raptor12762 <200654545+Raptor12762@users.noreply.github.com>
2025-05-21 10:36:16 -0400

    tw and sk img

--- src/routes/about/info/+page.svelte
+++ src/routes/about/info/+page.svelte
@@ -1,12 +1,17 @@
+<script lang="ts">
+    import tail from "@assets/tail.png"
+    import svel from "@assets/svel.png"
+</script>
+
 <!--Shows what we used to make this website possible as well as our wireframe and flowchart-->
 <div class="root-grid-box grid size-full gap-x-3 bg-[#EDCFFF]">
     <!--Sveltkit, quick info about what it is and what you can do, add the Svelt logo and have it link to Sveltkit on Google-->
     <div class="row-start-1 col-span-1 w-[75%] ml-auto mr-auto rounded-xl bg-[#9013d9] text-center self-center text-white px-10 py-10">
-        01
+        <img src={svel} alt="Svelte Kit">
     </div>
     <!--Tailwind, what is tailwind, how does it work, similarities to css, add tailwind logo with a link to tailwind's site-->
     <div class="row-start-2 col-span-1 w-[75%] ml-auto mr-auto rounded-xl bg-[#9013d9] text-center self-center text-white px-10 py-10">
-        02
+        <img src={tail} alt="Tailwind">
     </div>
     <!--Early wireframe of our site, have image, maybe a little bit of text to give context-->
     <div class="row-start-1 col-span-1 w-[75%] ml-auto mr-auto rounded-xl bg-[#9013d9] text-center self-center text-white px-10 py-10">
